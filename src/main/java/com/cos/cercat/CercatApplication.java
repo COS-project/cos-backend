3540b2774ae7543205bfa689e09c5fd0b1da5eef
강지원 <kangji149@naver.com>
2024-01-04 19:46:16 +0900

    feat : add jpa auditing
    * JPA 오디팅 추가

--- src/main/java/com/cos/cercat/CercatApplication.java
+++ src/main/java/com/cos/cercat/CercatApplication.java
@@ -2,8 +2,10 @@ package com.cos.cercat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CercatApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cos/cercat/common/entity/BaseTimeEntity.java
+++ src/main/java/com/cos/cercat/common/entity/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.cos.cercat.common;
+package com.cos.cercat.common.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
