7d2d07ffcb2714569e88c2e9e5b3aa6615b12bab
강지원 <kangji149@naver.com>
2024-01-04 11:28:40 +0900

    feat : add exception handling
    * 예외 처리 및 공통 Response 구현

---
+++ src/main/java/com/cos/cercat/common/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.cos.cercat.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@Getter
+public abstract class BaseTimeEntity {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    protected LocalDateTime modifiedAt;
+
+}
---
+++ src/main/java/com/cos/cercat/common/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.cos.cercat.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    MOCK_EXAM_NOT_FOUND(HttpStatus.NOT_FOUND, "mock exam not founded"),
+    INCORRECT_FILE_FORMAT(HttpStatus.BAD_REQUEST, "incorrect file format"),
+    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "invalid password"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "bad request"),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "unauthorized user"),
+    IMAGE_UPLOAD_FAIL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "image upload fail"),
+    NO_PERMISSION_ERROR(HttpStatus.FORBIDDEN, "user have no permission"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "there is internal server error");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
