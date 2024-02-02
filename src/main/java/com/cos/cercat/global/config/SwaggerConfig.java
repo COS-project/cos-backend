ad9cb68afefce56b7961ea14db8d32a455936756
강지원 <kangji149@naver.com>
2024-02-03 03:48:15 +0900

    feat : persist auth setting
    * 새로고침시에도 Auth토큰이 유지되도록 설정
    * 따라서 API 다시 그룹핑

--- src/main/java/com/cos/cercat/global/config/SwaggerConfig.java
+++ src/main/java/com/cos/cercat/global/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.SwaggerUiOAuthProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,77 +15,85 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public GroupedOpenApi post() {
-//        return GroupedOpenApi.builder()
-//                .group("게시글")
-//                .packagesToScan("com.cos.cercat.board.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi alarm() {
-//        return GroupedOpenApi.builder()
-//                .group("알림")
-//                .packagesToScan("com.cos.cercat.alarm.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi certificate() {
-//        return GroupedOpenApi.builder()
-//                .group("자격증")
-//                .packagesToScan("com.cos.cercat.certificate.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    GroupedOpenApi examReview() {
-//        return GroupedOpenApi.builder()
-//                .group("따끈 후기")
-//                .packagesToScan("com.cos.cercat.examReview.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi comment() {
-//        return GroupedOpenApi.builder()
-//                .group("댓글")
-//                .packagesToScan("com.cos.cercat.comment.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi learning() {
-//        return GroupedOpenApi.builder()
-//                .group("학습")
-//                .packagesToScan("com.cos.cercat.learning.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi like() {
-//        return GroupedOpenApi.builder()
-//                .group("좋아요")
-//                .packagesToScan("com.cos.cercat.like.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi mockExam() {
-//        return GroupedOpenApi.builder()
-//                .group("모의고사")
-//                .packagesToScan("com.cos.cercat.mockExam.api")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi user() {
-//        return GroupedOpenApi.builder()
-//                .group("유저")
-//                .packagesToScan("com.cos.cercat.user.api")
-//                .build();
-//    }
+    @Bean
+    public GroupedOpenApi board() {
+        return GroupedOpenApi.builder()
+                .group("게시글")
+                .packagesToScan("com.cos.cercat.board.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi alarm() {
+        return GroupedOpenApi.builder()
+                .group("알림")
+                .packagesToScan("com.cos.cercat.alarm.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi certificate() {
+        return GroupedOpenApi.builder()
+                .group("자격증")
+                .packagesToScan("com.cos.cercat.certificate.api")
+                .build();
+    }
+
+    @Bean
+    GroupedOpenApi examReview() {
+        return GroupedOpenApi.builder()
+                .group("따끈 후기")
+                .packagesToScan("com.cos.cercat.examReview.api")
+                .build();
+    }
+
+    @Bean
+    GroupedOpenApi mockExamResult() {
+        return GroupedOpenApi.builder()
+                .group("모의고사 결과")
+                .packagesToScan("com.cos.cercat.mockExamResult.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi comment() {
+        return GroupedOpenApi.builder()
+                .group("댓글")
+                .packagesToScan("com.cos.cercat.comment.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi learning() {
+        return GroupedOpenApi.builder()
+                .group("학습")
+                .packagesToScan("com.cos.cercat.learning.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi like() {
+        return GroupedOpenApi.builder()
+                .group("좋아요")
+                .packagesToScan("com.cos.cercat.like.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi mockExam() {
+        return GroupedOpenApi.builder()
+                .group("모의고사")
+                .packagesToScan("com.cos.cercat.mockExam.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi user() {
+        return GroupedOpenApi.builder()
+                .group("유저")
+                .packagesToScan("com.cos.cercat.user.api")
+                .build();
+    }
 
     @Bean
     public OpenAPI openAPI() {
@@ -113,7 +123,7 @@ public class SwaggerConfig {
                         .type(SecurityScheme.Type.APIKEY) // HTTP 방식
                         .scheme("bearer")
                         .in(SecurityScheme.In.HEADER)
-                        .bearerFormat("JWT"));// 토큰 형식을 지정하는 임의의 문자(Optional)
+                        .bearerFormat("JWT"));
 
         return new OpenAPI()
                 .info(info)
