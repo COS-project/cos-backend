6e177da9787b6aaa89fa26453a38226ee4c6e5bb
강지원 <kangji149@naver.com>
2024-02-02 10:22:04 +0900

    docs : Grouping APIs
    * API들을 그룹핑하여 문서 가독성 향상

--- src/main/java/com/cos/cercat/global/config/SwaggerConfig.java
+++ src/main/java/com/cos/cercat/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,12 +14,76 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
+    public GroupedOpenApi post() {
+        return GroupedOpenApi.builder()
+                .group("게시글")
+                .packagesToScan("com.cos.cercat.board.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi alarm() {
+        return GroupedOpenApi.builder()
+                .group("알림")
+                .packagesToScan("com.cos.cercat.alarm.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi certificate() {
+        return GroupedOpenApi.builder()
+                .group("자격증")
+                .packagesToScan("com.cos.cercat.certificate.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi comment() {
+        return GroupedOpenApi.builder()
+                .group("댓글")
+                .packagesToScan("com.cos.cercat.comment.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi learning() {
+        return GroupedOpenApi.builder()
+                .group("학습")
+                .packagesToScan("com.cos.cercat.learning.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi like() {
+        return GroupedOpenApi.builder()
+                .group("좋아요")
+                .packagesToScan("com.cos.cercat.like.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi mockExam() {
+        return GroupedOpenApi.builder()
+                .group("모의고사")
+                .packagesToScan("com.cos.cercat.mockExam.api")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi user() {
+        return GroupedOpenApi.builder()
+                .group("유저")
+                .packagesToScan("com.cos.cercat.user.api")
+                .build();
+    }
+
+    @Bean
     public OpenAPI openAPI() {
 
         Info info = new Info()
-                .version("v1.0.0")
-                .title("API 타이틀")
-                .description("API Description");
+                .version("1.0.0")
+                .title("Cercat API")
+                .description("Cercat API 문서입니다.");
 
         // SecuritySecheme명
         String accessTokenSchemeName = "Access-Token";
