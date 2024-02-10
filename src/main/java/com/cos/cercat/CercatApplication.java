8e6d939e819bb6e070dd50e211218bec37d17dca
강지원 <kangji149@naver.com>
2024-02-11 08:26:00 +0900

    feat : set timezone asia/seoul

--- src/main/java/com/cos/cercat/CercatApplication.java
+++ src/main/java/com/cos/cercat/CercatApplication.java
@@ -1,10 +1,13 @@
 package com.cos.cercat;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -15,4 +18,8 @@ public class CercatApplication {
 		SpringApplication.run(CercatApplication.class, args);
 	}
 
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
