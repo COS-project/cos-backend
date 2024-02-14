a15c77d375bc2eb9ace348a81df6b0fd078ec95d
강지원 <kangji149@naver.com>
2024-02-14 21:14:02 +0900

    docs : Add summary in API

--- src/main/java/com/cos/cercat/certificate/api/CertificateUpdateApi.java
+++ src/main/java/com/cos/cercat/certificate/api/CertificateUpdateApi.java
@@ -4,6 +4,7 @@ import com.cos.cercat.certificate.app.CertificateUpdateService;
 import com.cos.cercat.certificate.dto.request.InterestCertificateUpdateRequest;
 import com.cos.cercat.global.Response;
 import com.cos.cercat.user.dto.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,10 +24,11 @@ public class CertificateUpdateApi {
     private final CertificateUpdateService certificateUpdateService;
 
     @PutMapping("/interest-certificates")
+    @Operation(summary = "관심 자격증 수정")
     public Response<Void> updateInterestCertificates(@RequestBody List<InterestCertificateUpdateRequest> requests,
                                                      @AuthenticationPrincipal UserDTO user) {
         certificateUpdateService.updateInterestCertificates(requests, user.getId());
-        return Response.success("흥미 자격증 수정 성공");
+        return Response.success("관심 자격증 수정 성공");
     }
 
 }
