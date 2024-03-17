e9f00a1fbecca0af1f94f7410f953a3c0cd8e67d
강지원 <kangji149@naver.com>
2024-03-17 15:22:24 +0900

    feat : setting topic in prod

--- src/main/java/com/cos/cercat/post/app/search/EventDispatcher.java
+++ src/main/java/com/cos/cercat/post/app/search/EventDispatcher.java
@@ -20,7 +20,7 @@ public class EventDispatcher {
 
     private final EventHandlerFactory eventHandlerFactory;
 
-    @KafkaListener(topics = {"cercat.cercat_test.post", "cercat.cercat_test.post_comment"})
+    @KafkaListener(topics = {"${spring.kafka.topic.debezium}"})
     public void handleEvents(List<ConsumerRecord<String, DebeziumEvent>> records,
                              Acknowledgment acknowledgment) {
         log.info("Request to process {} records", records.size());
