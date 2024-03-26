7ff7a2a96b12e23d6cb6cddacf2771a791e8e9f4
강지원 <kangji149@naver.com>
2024-03-27 02:28:35 +0900

    chore : test

--- cercat-application/app-api/src/main/java/com/cos/cercat/apis/alarm/app/usecase/AlarmFetchUseCase.java
+++ cercat-application/app-api/src/main/java/com/cos/cercat/apis/alarm/app/usecase/AlarmFetchUseCase.java
@@ -23,7 +23,6 @@ public class AlarmFetchUseCase {
     @Transactional
     public List<AlarmResponse> getAlarms(Long userId) {
         User user = userService.getUser(userId);
-
         List<AlarmResponse> alarmResponses = alarmService.getAlarms(user).stream().map(AlarmResponse::from).toList();
         alarmService.markAllAsRead(user); //안읽은 모든 알림을 읽음으로 업데이트
         log.info("user - {} 읽지 않은 알림 조회 ", user.getEmail());
