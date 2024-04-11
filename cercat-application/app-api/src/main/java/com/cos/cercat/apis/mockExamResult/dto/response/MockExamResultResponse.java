7466c5981787829bf3e5f3e06a19432562ae988c
강지원 <kangji149@naver.com>
2024-04-11 11:32:07 +0900

    chore : mockExamResponse include createdAt
    - MockExamResponse에 시험날짜 추가

--- cercat-application/app-api/src/main/java/com/cos/cercat/apis/mockExamResult/dto/response/MockExamResultResponse.java
+++ cercat-application/app-api/src/main/java/com/cos/cercat/apis/mockExamResult/dto/response/MockExamResultResponse.java
@@ -3,19 +3,23 @@ package com.cos.cercat.apis.mockExamResult.dto.response;
 import com.cos.cercat.apis.mockExam.dto.response.MockExamResponse;
 import com.cos.cercat.domain.MockExamResult;
 
+import java.time.LocalDateTime;
+
 
 public record MockExamResultResponse(
         Long mockExamResultId,
         Integer round,
         MockExamResponse mockExam,
-        Integer totalScore
+        Integer totalScore,
+        LocalDateTime createdAt
 ) {
     public static MockExamResultResponse from(MockExamResult entity) {
         return new MockExamResultResponse(
                 entity.getId(),
                 entity.getRound(),
                 MockExamResponse.from(entity.getMockExam()),
-                entity.getTotalScore()
+                entity.getTotalScore(),
+                entity.getCreatedAt()
         );
     }
 }
