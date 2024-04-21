1fd8130059b37c096a5fa9cd0f3c3d0f63db34de
강지원 <kangji149@naver.com>
2024-04-21 17:39:28 +0900

    refactor : add v2 api

--- cercat-application/app-api/src/main/java/com/cos/cercat/apis/post/api/CreatePostApi.java
+++ cercat-application/app-api/src/main/java/com/cos/cercat/apis/post/api/CreatePostApi.java
@@ -1,11 +1,10 @@
 package com.cos.cercat.apis.post.api;
 
-import com.cos.cercat.apis.post.app.usecase.PostCreateUseCase;
 import com.cos.cercat.apis.post.dto.request.PostCreateRequest;
+import com.cos.cercat.common.domain.Response;
 import com.cos.cercat.domain.certificate.TargetCertificate;
 import com.cos.cercat.domain.post.CreatePostService;
 import com.cos.cercat.domain.post.PostType;
-import com.cos.cercat.common.domain.Response;
 import com.cos.cercat.domain.user.TargetUser;
 import com.cos.cercat.dto.UserDTO;
 import com.cos.cercat.gcs.FileUploader;
@@ -21,20 +20,28 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 @Tag(name = "게시글 생성 API")
-public class PostCreateApi {
+public class CreatePostApi {
 
-    private final PostCreateUseCase postCreateUseCase;
+    private final CreatePostService createPostService;
+    private final FileUploader fileUploader;
 
     @PostMapping(path = "/certificates/{certificateId}/{postType}/posts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "게시글 생성")
-    public Response<Void> createPost(@PathVariable Long certificateId,
-                                     @PathVariable PostType postType,
-                                     @RequestPart PostCreateRequest request,
-                                     @RequestPart(required = false) List<MultipartFile> images,
-                                     @AuthenticationPrincipal UserDTO currentUser) {
-        postCreateUseCase.createPost(certificateId, postType, request, images, currentUser.getId());
+    public Response<Void> createPost2(@PathVariable Long certificateId,
+                                      @PathVariable PostType postType,
+                                      @RequestPart PostCreateRequest request,
+                                      @RequestPart(required = false) List<MultipartFile> images,
+                                      @AuthenticationPrincipal UserDTO currentUser) {
+        List<String> imageUrls = fileUploader.uploadFileInStorage2(images);
+        createPostService.createCommentaryPost(
+                TargetUser.from(currentUser.getId()),
+                TargetCertificate.from(certificateId),
+                request.toContent(postType, imageUrls),
+                request.toMockExamSession(),
+                request.questionSequence()
+        );
         return Response.success("게시글 생성 완료");
     }
 }
