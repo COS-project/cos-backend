91cbcabbc960b4bf65e873a877657df3934635e6
Jiwon <rkdwldnjs878@gmail.com>
2025-04-02 04:40:26 +0900

    fix: 문서 URL 수정

--- applications/app-alarm/src/main/java/com/cos/cercat/alarm/config/SwaggerConfig.java
+++ applications/app-alarm/src/main/java/com/cos/cercat/alarm/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
