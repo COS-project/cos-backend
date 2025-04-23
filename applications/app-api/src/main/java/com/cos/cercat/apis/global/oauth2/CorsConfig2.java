53e419dcb49748007a6ee1161e7399f65541080b
Jiwon <rkdwldnjs878@gmail.com>
2025-04-23 23:37:10 +0900

    feat: add credential

---
+++ applications/app-api/src/main/java/com/cos/cercat/apis/global/oauth2/CorsConfig2.java
@@ -0,0 +1,23 @@
+package com.cos.cercat.apis.global.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig2 implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("DELETE", "GET", "POST", "PATCH", "PUT")
+                .allowedHeaders("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization", "Access-Token", "Refresh-Token")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
