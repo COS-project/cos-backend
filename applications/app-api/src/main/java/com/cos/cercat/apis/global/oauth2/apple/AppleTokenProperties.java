016c1705f3326961533cb81c71c8d038fe99895a
Jiwon <rkdwldnjs878@gmail.com>
2025-04-23 20:38:27 +0900

    chore: 비밀키 추가

--- applications/app-api/src/main/java/com/cos/cercat/apis/global/oauth2/apple/AppleTokenProperties.java
+++ applications/app-api/src/main/java/com/cos/cercat/apis/global/oauth2/apple/AppleTokenProperties.java
@@ -1,12 +1,14 @@
 package com.cos.cercat.apis.global.oauth2.apple;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "apple")
 @Getter
+@Setter
 public class AppleTokenProperties {
     private String path;
     private String url;
