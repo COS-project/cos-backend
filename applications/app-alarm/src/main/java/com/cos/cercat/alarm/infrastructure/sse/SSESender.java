3cb33b149aae30d764fb6b845ee79c82e99d3e1c
Jiwon <rkdwldnjs878@gmail.com>
2025-04-25 00:47:16 +0900

    chore: SSE 로깅 추가

--- applications/app-alarm/src/main/java/com/cos/cercat/alarm/infrastructure/sse/SSESender.java
+++ applications/app-alarm/src/main/java/com/cos/cercat/alarm/infrastructure/sse/SSESender.java
@@ -5,11 +5,13 @@ import com.cos.cercat.domain.alarm.Alarm;
 import java.io.IOException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SSESender {
 
     public final static String EVENT_NAME = "alarm";
@@ -27,6 +29,7 @@ public class SSESender {
                     .name(EVENT_NAME)
                     .data(alarm)
             );
+            log.info("SSE 전송 완료 receiverId - {}, {}", receiverId, alarm);
         } catch (IOException e) {
             sseEmitterRepository.delete(receiverId);
             throw AlarmSendException.EXCEPTION;
